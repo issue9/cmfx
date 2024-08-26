49a5fffc5afca800244f169db97757804a157c7a
caixw <xnotepad@gmail.com>
2024-08-27 01:59:13 +0800

    refactor(admin/components): 作了以下两个修改
    - 修正 splitbutton 没有正确关闭子菜单的错误；
    - 调整表格中导出菜单的样式；

--- admin/src/components/button/split.tsx
+++ admin/src/components/button/split.tsx
@@ -4,8 +4,9 @@
 
 import { createSignal, For, JSX, Match, mergeProps, splitProps, Switch } from 'solid-js';
 
+import { Corner } from '@/components/base';
 import { Dropdown } from '@/components/dropdown';
-import { Props as BaseProps, default as Button, defaultProps } from './button';
+import { Props as BaseProps, default as Button, defaultProps as defaultBaseProps } from './button';
 import { default as Group } from './group';
 import { ClickFunc } from './types';
 
@@ -13,11 +14,18 @@ type Item = { type: 'divider' } | {
     type: 'item';
     label: JSX.Element;
     onClick: ClickFunc;
+    disabled?: boolean;
 };
 
 export interface Props extends BaseProps {
+    pos?: Corner;
     menus: Array<Item>;
 }
+
+const defaultProps = {
+    ...defaultBaseProps,
+    pos: 'bottomleft' as Corner
+};
 
 /**
  * 带有一个默认操作的一组按钮
@@ -29,14 +37,14 @@ export default function(props: Props) {
 
     const [visible, setVisible] = createSignal(false);
 
-    const [_, btnProps] = splitProps(props, ['style', 'rounded', 'disabled', 'palette']);
+    const [_, btnProps] = splitProps(props, ['style', 'rounded', 'disabled', 'palette', 'pos']);
 
     const activator = <Group style={props.style} rounded={props.rounded} disabled={props.disabled}>
         <Button {...btnProps}>{props.children}</Button>
         <Button icon={/*@once*/true} onClick={()=>setVisible(!visible())}>keyboard_arrow_down</Button>
     </Group>;
 
-    return <Dropdown class="c--split-button_content" palette={props.palette} activator={activator} visible={visible()} setVisible={setVisible}>
+    return <Dropdown class="c--split-button_content" pos={props.pos} palette={props.palette} activator={activator} visible={visible()} setVisible={setVisible}>
         <For each={props.menus}>
             {(item)=>(
                 <Switch>
@@ -44,7 +52,10 @@ export default function(props: Props) {
                         <hr class="text-palette-bg-low" />
                     </Match>
                     <Match when={item.type === 'item'}>
-                        <button onClick={(item as any).onClick}>{(item as any).label}</button>
+                        <button disabled={(item as any).disabled} class="whitespace-nowrap c--icon-container" onClick={() => {
+                            (item as any).onClick();
+                            setVisible(false);
+                        }}>{(item as any).label}</button>
                     </Match>
                 </Switch>
             )}
