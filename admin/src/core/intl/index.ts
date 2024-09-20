bb360cbe0c9a8217d08ed56ee25f00f6c623a900
caixw <xnotepad@gmail.com>
2024-09-21 02:06:07 +0800

    feat(admin/core/intl): 添加与翻译对象相关的功能

---
+++ admin/src/core/intl/index.ts
@@ -0,0 +1,7 @@
+// SPDX-FileCopyrightText: 2024 caixw
+//
+// SPDX-License-Identifier: MIT
+
+export { flatten as flattenDict } from './dict';
+export type { Dict, Keys as DictKeys, Flatten as FlattenDict } from './dict';
+
