53d00b579ebc200ae004a1298b136cc6db667a58
caixw <xnotepad@gmail.com>
2024-10-23 11:30:18 +0800

    feat(admin/core/time): 添加 createTimer 用于创建一个可暂停的计时器

---
+++ admin/src/core/time/time.spec.ts
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2024 caixw
+//
+// SPDX-License-Identifier: MIT
+
+import { expect, test } from 'vitest';
+import { createTimer, sleep } from './time';
+
+test('createTimer', async() => {
+    let isDone = false;
+    let tick = 0;
+
+    const t = createTimer(() => {
+        isDone = true;
+    }, 500, 100, (_: number) => { tick++; });
+
+    expect(isDone).toBeFalsy();
+    expect(tick).toEqual(0);
+
+    t.pause();
+    await sleep(800);
+    expect(isDone).toBeFalsy();
+    expect(tick).toEqual(0);
+
+    t.start();
+    await sleep(200);
+    expect(isDone).toBeFalsy();
+    expect(tick>0).toBeTruthy();
+
+    await sleep(700);
+    expect(isDone).toBeTruthy();
+    expect(tick>4).toBeTruthy();
+
+    expect(()=>createTimer(() => { }, 500, 500)).toThrow('timeout 的值最起码是 2*step');
+});
--- admin/src/core/time/time.ts
+++ admin/src/core/time/time.ts
@@ -9,3 +9,51 @@
 export function sleep(ms: number) {
     return new Promise(resolve=>setTimeout(resolve, ms));
 }
+
+/**
+ * 创建一个可暂停的计时器
+ *
+ * @param fn 计时器到点时需要执行的函数；
+ * @param timeout 计时器的时间，单位为 ms；
+ * @param step 计时器可暂停的最短时间，单位为 ms。该值应该小于 timeout，否则为抛出异常；
+ * @param tick 每一个 step 时间调用的方法，函数签名为 {(timeout: number): void}，timeout 表示剩余的时间；
+ */
+export function createTimer(fn: { (): void; }, timeout: number, step: number, tick?: { (t: number): void; }) {
+    if (timeout/step < 2) {
+        throw 'timeout 的值最起码是 2*step';
+    }
+
+    const createInterval = () => {
+        return setInterval(() => {
+            timeout -= step;
+
+            if (tick) {
+                tick(timeout);
+            }
+
+            if (timeout <= 0) {
+                clearInterval(timer);
+                fn();
+            }
+        }, step);
+    };
+
+    let timer = createInterval();
+
+    return {
+        /**
+         * 销毁计时器
+         */
+        stop() { clearInterval(timer); },
+
+        /**
+         * 暂停计时器，之后可用 restart 继续
+         */
+        pause() { clearInterval(timer); },
+
+        /**
+         * 开始计时
+         */
+        start() { timer = createInterval();},
+    };
+}
