ec0a388709e7eda92a237dde70934b1ee7e1900d
caixw <xnotepad@gmail.com>
2024-11-04 11:15:22 +0800

    feat(admin/components/backtop): Props.Scroller 允许以 ID 作为值

--- admin/src/components/backtop/demo.tsx
+++ admin/src/components/backtop/demo.tsx
@@ -5,6 +5,7 @@
 import { For } from 'solid-js';
 
 import { Demo, paletteSelector } from '@/components/base/demo';
+import { IconSymbol } from '@/components/icon';
 import BackTop from './backtop';
 
 export default function() {
@@ -16,13 +17,24 @@ export default function() {
 
     let scroller: HTMLElement;
     return <Demo settings={paletteS} stages={
-        <div ref={el=>scroller=el} class="overflow-y-scroll w-full h-[500px] border border-palette-fg mt-10">
-            <For each={len}>
-                {(i)=>(
-                    <>{i} <br /></>
-                )}
-            </For>
-            <BackTop scroller={scroller!} palette={palette()} class="mb-10" />
-        </div>
+        <>
+            <div ref={el=>scroller=el} class="overflow-y-scroll w-1/3 h-[500px] border border-palette-fg mt-10">
+                <For each={len}>
+                    {(i)=>(
+                        <>{i} <br /></>
+                    )}
+                </For>
+                <BackTop scroller={scroller!} palette={palette()} class="mb-10 left-[300px] !bottom-4" style={{'right':'unset'}} />
+            </div>
+            
+            <div ref={el=>scroller=el} class="overflow-y-scroll w-1/3 h-[500px] border border-palette-fg mt-10">
+                <For each={len}>
+                    {(i)=>(
+                        <>{i} <br /></>
+                    )}
+                </For>
+                <BackTop scroller={scroller!} palette={palette()} class="mb-10">{'navigation' as IconSymbol}</BackTop>
+            </div>
+        </>
     } />;
 }
