72d7c2ee2c0eaee6553c009327118d76db8818f7
caixw <xnotepad@gmail.com>
2025-03-27 11:21:08 +0800

    chore: v0.8.14

--- admin/src/index.ts
+++ admin/src/index.ts
@@ -1,9 +1,21 @@
-// SPDX-FileCopyrightText: 2024 caixw
+// SPDX-FileCopyrightText: 2024-2025 caixw
 //
 // SPDX-License-Identifier: MIT
+
+/**
+ * 提供了后台管理界面的基本功能
+ *
+ * @example
+ * ```ts
+ * createApp('app', {
+ *     title: 'title',
+ *     // ...
+ * })
+ * ```
+ * @module
+ */
 
 import './style.css';
 
 export * from './app';
 export type { Messages as InternalMessages } from './messages';
-
--- cmfx/cmfx.go
+++ cmfx/cmfx.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Version 表示当前框架的版本
-const Version = "0.8.13"
+const Version = "0.8.14"
 
 // 400
 const (
