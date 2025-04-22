1a54021a3044050655e96aa64335bd5fd5c757bf
caixw <xnotepad@gmail.com>
2025-04-22 23:23:55 +0800

    refactor: 排除一些用不着的测试文件

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,19 +5,23 @@
 import solidPlugin from 'vite-plugin-solid';
 import { defineConfig } from 'vitest/config';
 
+// 需要排除的文件，同时作用在 test.exclude 和 test.coverage.exclude 字段。
+const exclude = ['**/lib/**', '**/node_modules/**', 'cmd/**', 'vite.config.ts', 'vitest.config.ts', 'eslint.config.mjs'];
+
 export default defineConfig({
     test: {
         workspace: ['packages/core', 'packages/components', 'packages/admin'],
         environment: 'jsdom',
-        exclude: ['**/lib/**', '**/node_modules/**'],
+        exclude: exclude,
         coverage: {
-            exclude: ['lib/**', 'vite.config.ts', 'node_modules/**'],
+            provider: 'v8',
+            exclude: exclude,
             reporter: ['text', 'json-summary', ['json', { file: 'report.json' }]],
             reportOnFailure: true,
         }
     },
     plugins: [
-        solidPlugin() // 必须要有，否则所有测试的 isserver 始终为 true。
+        solidPlugin() // 必须要有，否则所有测试的 isServer 始终为 true。
     ]
 });
 
