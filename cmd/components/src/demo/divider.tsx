5748f537b1a6adb4146fd9f64b60c5fd49f8e028
caixw <xnotepad@gmail.com>
2025-05-14 23:15:34 +0800

    feat(pakcages/components/divider): 支持 layout 属性

--- cmd/components/src/demo/divider.tsx
+++ cmd/components/src/demo/divider.tsx
@@ -5,17 +5,17 @@
 import { Divider, DividerProps } from '@cmfx/components';
 import { createSignal, For } from 'solid-js';
 
-import { Demo, paletteSelector, boolSelector } from './base';
+import { Demo, layoutSelector, paletteSelector } from './base';
 
 export default function() {
     const [paletteS, palette] = paletteSelector();
-    const [verticalS, vertical] = boolSelector('vertical');
+    const [layoutS, layout] = layoutSelector('布局', 'vertical');
     const [pos, setPos] = createSignal<DividerProps['pos']>('start');
 
     return <Demo settings={
         <>
             {paletteS}
-            {verticalS}
+            {layoutS}
             <fieldset class="border-2">
                 <legend>位置</legend>
                 <For each={new Array<DividerProps['pos']>('start', 'center', 'end')}>
@@ -29,11 +29,33 @@ export default function() {
         </>
     }>
         <div class="w-56 h-56">
-            <Divider vertical={vertical()} palette={palette()} pos={pos()}><span class="c--icon material-symbols-outlined">face</span>起始位置</Divider>
+            <Divider layout={layout()} palette={palette()} pos={pos()}>
+                <span class="c--icon material-symbols-outlined">face</span>起始位置
+            </Divider>
         </div>
 
         <div class="w-56 h-56">
-            <Divider vertical={vertical()} palette={palette()} pos={pos()}></Divider>
+            <Divider layout={layout()} palette={palette()} pos={pos()}>
+                <span class="c--icon material-symbols-outlined">face</span>english
+            </Divider>
+        </div>
+
+        <div class="w-56 h-56">
+            <Divider layout={layout()} palette={palette()} pos={pos()}>
+                <span style={{'writing-mode': 'vertical-rl', 'text-orientation': 'upright'}}>起始位置<span>111</span></span>
+            </Divider>
+        </div>
+
+        <div class="w-56 h-56">
+            <Divider layout={layout()} palette={palette()} pos={pos()}>
+                <span style={{'writing-mode': 'vertical-rl', 'text-orientation': 'upright', 'display': 'flex', 'align-items': 'center'}}>
+                    english<span class="c--icon material-symbols-outlined">face</span>
+                </span>
+            </Divider>
+        </div>
+
+        <div class="w-56 h-56">
+            <Divider layout={layout()} palette={palette()} pos={pos()}></Divider>
         </div>
     </Demo>;
 }
