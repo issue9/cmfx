87cb423a3d437480a46d3a42e2a2ac72b72bf345
caixw <xnotepad@gmail.com>
2025-05-14 09:40:08 +0800

    fix: 修正 vitest 脚本中的配置错误

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,28 +2,41 @@
 //
 // SPDX-License-Identifier: MIT
 
+import { defineConfig as defineViteConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, mergeConfig } from 'vitest/config';
 
 // 需要排除的文件，同时作用在 test.exclude 和 test.coverage.exclude 字段。
-const exclude = ['**/lib/**', '**/node_modules/**', 'cmd/**', '**/vite.config.ts', '**/vitest.config.ts', '**/vitest_setup.ts', 'eslint.config.mjs'];
+const exclude = [
+    '**/index.ts',
+    '**/index.tsx',
+    '**/lib/**',
+    '**/node_modules/**',
+    '**/vite.config.ts',
+    '**/vitest.config.ts',
+    '**/vitest_setup.ts'
+];
 
-export default defineConfig({
-    test: {
-        workspace: ['packages/core', 'packages/components', 'packages/admin'],
-        environment: 'jsdom',
-        exclude: exclude,
-        coverage: {
-            clean: false,
-            reportsDirectory: './coverage',
-            provider: 'v8',
+export default mergeConfig(
+    defineViteConfig({
+        plugins: [
+            solidPlugin() // 必须要有，否则所有测试的 isServer 始终为 true。
+        ]
+    }),
+    defineConfig({
+        test: {
+            workspace: ['packages/core', 'packages/components', 'packages/admin'],
+            environment: 'jsdom',
             exclude: exclude,
-            reporter: ['text', 'text-summary', ['json', { file: 'ts.json' }]],
-            reportOnFailure: true,
-        }
-    },
-    plugins: [
-        solidPlugin() // 必须要有，否则所有测试的 isServer 始终为 true。
-    ]
-});
-
+            coverage: {
+                include: ['packages/**/*.ts', 'packages/**/*.tsx'],
+                clean: false,
+                reportsDirectory: './coverage',
+                provider: 'v8',
+                exclude: exclude,
+                reporter: ['text', 'text-summary', ['json', { file: 'ts.json' }]],
+                reportOnFailure: true,
+            }
+        },
+    })
+);
