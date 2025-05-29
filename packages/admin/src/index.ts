a7390a1bf5a68c86465b43d5d4b6d571a9c4ece6
caixw <xnotepad@gmail.com>
2025-05-29 11:40:45 +0800

    docs(packages/packages): 删除无用的注释

--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -2,19 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-/**
- * 提供了后台管理界面的基本功能
- *
- * @example
- * ```ts
- * createApp('app', {
- *     title: 'title',
- *     // ...
- * })
- * ```
- * @module
- */
-
 import './style.css';
 export * from './app';
 export * from './components';
