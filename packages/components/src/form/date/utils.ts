053dbdc08e6d22b779d09e259e0152b7d1bd0eb3
caixw <xnotepad@gmail.com>
2025-07-24 01:33:02 +0800

    feat(packages/components): 添加 WeekPicker

---
+++ packages/components/src/form/date/utils.ts
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 caixw
+//
+// SPDX-License-Identifier: MIT
+
+import { adjustPopoverPosition } from '@cmfx/core';
+
+export function togglePop(anchor: Element, popElem: HTMLElement): boolean {
+    const ab = anchor.getBoundingClientRect();
+    const ret = popElem.togglePopover();
+    adjustPopoverPosition(popElem, ab, 2);
+    return ret;
+}
--- packages/components/src/form/date/week.tsx
+++ packages/components/src/form/date/week.tsx
@@ -3,58 +3,41 @@
 // SPDX-License-Identifier: MIT
 
 import { createMemo, createSignal, createUniqueId, JSX, mergeProps, Show, splitProps, untrack } from 'solid-js';
-import IconArrowRight from '~icons/bxs/right-arrow';
 import IconClose from '~icons/material-symbols/close';
 import IconExpandAll from '~icons/material-symbols/expand-all';
 
 import { joinClass } from '@/base';
-import { Button } from '@/button';
-import { useLocale } from '@/context';
-import { DateRangePanel, RangeValueType } from '@/datetime';
+import { WeekPanel } from '@/datetime';
+import { WeekValueType } from '@/datetime/dateview';
 import { Accessor, calcLayoutFieldAreas, Field, fieldArea2Style, FieldHelpArea } from '@/form/field';
-import { IconComponent } from '@/icon';
-import { presetProps as basePresetProps, Props as PickerProps } from './date';
+import { Props as PickerProps, presetProps } from './date';
 import styles from './style.module.css';
 import { togglePop } from './utils';
 
-export interface Props extends Omit<PickerProps, 'accessor'> {
-    /**
-     * 中间的箭头
-     */
-    arrowIcon?: IconComponent;
-
-    accessor: Accessor<RangeValueType | undefined>;
-
-    /**
-     * 是否显示右侧快捷选择栏
-     */
-    shortcuts?: boolean;
+export interface Props extends Omit<PickerProps, 'accessor' | 'accentPalette' | 'time'> {
+    accessor: Accessor<WeekValueType | undefined>;
 }
 
-const presetProps = {
-    ...basePresetProps,
-    arrowIcon: IconArrowRight,
-} as const;
-
-export function DateRangePicker(props: Props): JSX.Element {
+/**
+ * 周数选择组件
+ */
+export function WeekPicker(props: Props): JSX.Element {
     props = mergeProps(presetProps, props);
-    const l = useLocale();
 
     const [panelProps, _] = splitProps(props,
-        ['time', 'weekBase', 'weekend', 'disabled', 'readonly', 'palette', 'min', 'max', 'shortcuts']);
+        ['weekBase', 'weekend', 'disabled', 'readonly', 'palette', 'min', 'max']);
 
     let panelRef: HTMLElement;
     let anchorRef: HTMLElement;
 
     const [hover, setHover] = createSignal(false);
 
-    const change = (val?: RangeValueType) => {
+    const change = (val?: WeekValueType) => {
         props.accessor.setValue(val);
+        panelRef.hidePopover();
     };
 
-    const formater = createMemo(() => {
-        return props.time ? l.datetimeFormat().format : l.dateFormat().format;
-    });
+    const format = (val: WeekValueType) => { return val ? `${val[0]}-${val[1]}` : ''; };
 
     const id = createUniqueId();
     const areas = createMemo(() => calcLayoutFieldAreas(props.layout!, props.accessor.hasHelp(), !!props.label));
@@ -72,12 +55,7 @@ export function DateRangePicker(props: Props): JSX.Element {
         >
             <input id={id} readOnly disabled={props.disabled} placeholder={props.placeholder}
                 class={joinClass(styles.input, styles.range)}
-                value={formater()(props.accessor.getValue()![0])}
-            />
-            {props.arrowIcon!({ class: 'px-1 shrink-0' })}
-            <input readOnly disabled={props.disabled} placeholder={props.placeholder}
-                class={joinClass(styles.input, styles.range)}
-                value={formater()(props.accessor.getValue()![1])}
+                value={format(props.accessor.getValue()!)}
             />
 
             <Show when={hover() && props.accessor.getValue()} fallback={<IconExpandAll class="shrink-0" />}>
@@ -88,23 +66,10 @@ export function DateRangePicker(props: Props): JSX.Element {
             </Show>
         </div>
 
-        <fieldset popover="auto" disabled={props.disabled} ref={el => panelRef = el} class={styles.panel} aria-haspopup>
-
-            <DateRangePanel class={styles['dt-panel']} {...panelProps}
-                value={untrack(props.accessor.getValue)} onChange={change}
-            />
-
-            <div class={joinClass(styles.actions, '!justify-end')}>
-                <Button kind='flat' class='py-0 px-1' onClick={() => {
-                    props.accessor.setValue([undefined, undefined]);
-                    panelRef.hidePopover();
-                }}>{l.t('_c.date.clear')}</Button>
-
-                <Button kind='flat' class='py-0 px-1' palette={props.accentPalette} onClick={() => {
-                    panelRef.hidePopover();
-                }}>{l.t('_c.ok')}</Button>
-            </div>
-        </fieldset>
+        <WeekPanel {...panelProps} ref={el => panelRef = el}
+            popover="auto" disabled={props.disabled} aria-haspopup
+            value={untrack(props.accessor.getValue)} onChange={change}
+        />
 
         <Show when={areas().helpArea}>
             {area => <FieldHelpArea area={area()} getError={props.accessor.getError} help={props.help} />}
