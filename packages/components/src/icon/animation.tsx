5f1a00ef22af907e1a34277e662d6a8120dd48dc
caixw <xnotepad@gmail.com>
2025-07-25 19:45:58 +0800

    feat(packages/components/icon): 添加 AnimationIcon 动画图标

---
+++ packages/components/src/icon/animation.tsx
@@ -0,0 +1,96 @@
+// SPDX-FileCopyrightText: 2025 caixw
+//
+// SPDX-License-Identifier: MIT
+
+import { createEffect, createSignal, createUniqueId, JSX, onMount } from 'solid-js';
+import { template } from 'solid-js/web';
+import { bundleSvgsString, SVGMorpheus } from 'svg-morpheus-ts';
+
+import { BaseProps, transitionDuration } from '@/base';
+import { IconComponent } from './icon';
+
+export interface Ref {
+    /**
+     * 跳转至新图标
+     *
+     * @param gid 跳转至新图标的 ID，该值必须是图标集中的一个，如果不存在则不执行任何操作；
+     */
+    to(gid: string): void;
+}
+
+/**
+ * 可用的旋转方式
+ */
+export const animationIconRotations = ['clock', 'counterclock', 'none', 'random'] as const;
+
+/**
+ * 动画图标的旋转方式
+ */
+export type AnimationIconRotation = typeof animationIconRotations[number];
+
+export interface Props extends BaseProps {
+    /**
+     * 图标集
+     *
+     * 键名为图标的 ID；
+     * 键值为图标实例；
+     */
+    icons: Record<string, IconComponent>;
+
+    /**
+     * 默认显示的图标，如果未指定，则采用 {@link Props#icons} 中的最后一个。
+     */
+    preset?: string;
+
+    /**
+     * 缓动函数
+     *
+     * 如果需要自定义缓动函数，可以通过 svg-morpheus-ts 包使用 {@link SVGMorpheus#registerEasing} 方法进行注册。
+     */
+    easing?: string;
+
+    /**
+     * 旋转方式
+     */
+    rotation?: AnimationIconRotation;
+
+    ref: { (ref: Ref): void; };
+}
+
+/**
+ * 动画图标
+ *
+ * 可以一次性指定多个图标，通过 {@link Ref#to} 实现跳转到另一个图标且带有动画效果。
+ */
+export function AnimationIcon(props: Props): JSX.Element {
+    const [icons, setIcons] = createSignal<SVGSVGElement>();
+    const id = createUniqueId();
+
+    createEffect(async () => {
+        const maps: Record<string, string> = {};
+
+        Object.entries(props.icons).forEach((value) => {
+            maps[value[0]] = (value[1]({}) as HTMLElement)?.outerHTML;
+        });
+
+        const el = await bundleSvgsString(maps, {id, style: ''});
+        setIcons(template(el)().cloneNode(true) as SVGSVGElement);
+    });
+
+    onMount(() => {
+        requestIdleCallback(() => {
+            const m = new SVGMorpheus('#' + icons()?.id, {
+                iconId: props.preset,
+                duration: transitionDuration(300),
+                easing: props.easing,
+                rotation: props.rotation,
+            }, ()=>{
+                props.ref({
+                    to: (gid) => { m.to(gid); }
+                });
+            });
+        });
+    });
+
+    return <>{icons()}</>;
+}
--- packages/components/src/icon/index.ts
+++ packages/components/src/icon/index.ts
@@ -4,3 +4,7 @@
 
 export type { IconComponent, Props as IconComponentProps } from './icon';
 
+export { easings as animationIconEasings } from 'svg-morpheus-ts';
+export { AnimationIcon, animationIconRotations } from './animation';
+export type { Props as AnimationIconProps, Ref as AnimationIconRef, AnimationIconRotation } from './animation';
+
