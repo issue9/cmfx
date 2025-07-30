79d3b6957220344735388da1e6b39936da19b69f
caixw <xnotepad@gmail.com>
2025-07-30 16:17:06 +0800

    feat(packages/components): 添加 PropsError

---
+++ packages/components/src/base/errors.ts
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: 2025 caixw
+//
+// SPDX-License-Identifier: MIT
+
+/**
+ * 表示组件的属性字段错误
+ */
+export class PropsError extends Error {
+    readonly #prop: string;
+    readonly #msg: any;
+
+    /**
+     * 初始化表示组件属性错误的对象
+     *
+     * @param prop 组件属性名称；
+     * @param msg 错误信息；
+     */
+    constructor(prop: string, msg: any) {
+        super(msg);
+        this.name = 'PropsError';
+
+        this.#msg = msg;
+        this.#prop = prop;
+    }
+
+    /**
+     * 获取错误的属性名称
+     */
+    get prop(): string { return this.#prop; }
+
+    /**
+     * 获取错误信息
+     */
+    get message(): any { return this.#msg; }
+}
--- packages/components/src/base/index.ts
+++ packages/components/src/base/index.ts
@@ -12,4 +12,6 @@ export type { AvailableEnumType, Enum, Enums } from './enum';
 
 export type { Locale } from './locale';
 
+export type { PropsError } from './errors';
+
 export * from './theme';
