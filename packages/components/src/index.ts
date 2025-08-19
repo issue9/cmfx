ccc2fe59c3c5678f468501fcaa9dff533fdbbe6f
caixw <xnotepad@gmail.com>
2025-08-19 20:28:59 +0800

    fix(packages/components): 解决 safari 不支持 requestIdleCallback 的问题

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -4,6 +4,21 @@
 
 import './style.css';
 
+// TODO: safari 支持 requestIdleCallback，后期可删除。
+// https://caniuse.com/?search=requestIdleCallback
+window.requestIdleCallback = window.requestIdleCallback ||
+    function (cb) {
+        var start = Date.now();
+        return setTimeout(function () {
+            cb({
+                didTimeout: false,
+                timeRemaining: function () {
+                    return Math.max(0, 50 - (Date.now() - start));
+                },
+            });
+        }, 1);
+    };
+
 export * from './appbar';
 export * from './backtop';
 export * from './badge';
