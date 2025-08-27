3783816c086631baf27cff2ed92d46edeed5de65
caixw <xnotepad@gmail.com>
2025-08-27 14:14:40 +0800

    feat(core): calcPopoverPosition 和 adjustPopoverPosition 支持 rtl 模式

--- packages/core/src/dom/popover.spec.ts
+++ packages/core/src/dom/popover.spec.ts
@@ -15,7 +15,7 @@ describe('calcPopoverPosition', () => {
 
     const anchor = new DOMRect(5, 5, 15, 15);
 
-    test('left', () => {
+    test('left-start', () => {
         let p = calcPopoverPosition(elem, anchor, 'left', 'start');
         expect(p).toEqual({ x: -5, y: 5 });
 
@@ -23,7 +23,7 @@ describe('calcPopoverPosition', () => {
         expect(p).toEqual({ x: -6, y: 5 });
     });
 
-    test('right', () => {
+    test('right-end', () => {
         let p = calcPopoverPosition(elem, anchor, 'right', 'end');
         expect(p).toEqual({ x: 20, y: 5 });
 
@@ -31,7 +31,7 @@ describe('calcPopoverPosition', () => {
         expect(p).toEqual({ x: 21, y: 5 });
     });
 
-    test('top', () => {
+    test('top-start', () => {
         let p = calcPopoverPosition(elem, anchor, 'top', 'start');
         expect(p).toEqual({ x: 5, y: -5 });
 
@@ -39,11 +39,35 @@ describe('calcPopoverPosition', () => {
         expect(p).toEqual({ x: 5, y: -6 });
     });
 
-    test('bottom', () => {
+    test('bottom-center', () => {
         let p = calcPopoverPosition(elem, anchor, 'bottom', 'center');
         expect(p).toEqual({ x: 7.5, y: 20 });
 
         p = calcPopoverPosition(elem, anchor, 'bottom', 'center', 1);
         expect(p).toEqual({ x: 7.5, y: 21 });
+    });
+
+    test('bottom-center-rtl', () => {
+        let p = calcPopoverPosition(elem, anchor, 'bottom', 'center', 0, true);
+        expect(p).toEqual({ x: 7.5, y: 20 });
+
+        p = calcPopoverPosition(elem, anchor, 'bottom', 'center', 1, true);
+        expect(p).toEqual({ x: 7.5, y: 21 });
+    });
+
+    test('bottom-start-rtl', () => {
+        let p = calcPopoverPosition(elem, anchor, 'bottom', 'start', 0, true);
+        expect(p).toEqual({ x: 10, y: 20 });
+
+        p = calcPopoverPosition(elem, anchor, 'bottom', 'start', 1, true);
+        expect(p).toEqual({ x: 10, y: 21 });
+    });
+
+    test('bottom-end-rtl', () => {
+        let p = calcPopoverPosition(elem, anchor, 'bottom', 'end', 0, true);
+        expect(p).toEqual({ x: 5, y: 20 });
+
+        p = calcPopoverPosition(elem, anchor, 'bottom', 'end', 1, true);
+        expect(p).toEqual({ x: 5, y: 21 });
     });
 });
