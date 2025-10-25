5088867528c6a203aa93482f36a4e7fa557e1b4e
caixw <xnotepad@gmail.com>
2025-10-25 13:16:15 +0800

    deps: 更新前端依赖

--- packages/components/src/vitest_setup.ts
+++ packages/components/src/vitest_setup.ts
@@ -4,8 +4,11 @@
 
 import { vi } from 'vitest';
 
-global.ResizeObserver = vi.fn().mockImplementation(() => ({
-    observe: vi.fn(),
-    unobserve: vi.fn(),
-    disconnect: vi.fn(),
-}));
+Object.defineProperty(window, 'ResizeObserver', {
+    writable: true,
+    value: vi.fn().mockImplementation(() => ({
+        observe: vi.fn(),
+        unobserve: vi.fn(),
+        disconnect: vi.fn(),
+    })),
+});
