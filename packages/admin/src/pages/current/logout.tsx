179d0f30e9603897b2939f6e316ec174e65a79c1
caixw <xnotepad@gmail.com>
2025-11-12 02:07:06 +0800

    refactor(assets): 调整产品图标

--- packages/admin/src/pages/current/logout.tsx
+++ packages/admin/src/pages/current/logout.tsx
@@ -2,10 +2,9 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { Page } from '@cmfx/components';
+import { Page, IconCmfxBrandAnimate } from '@cmfx/components';
 import { useNavigate } from '@solidjs/router';
 import { JSX, onMount } from 'solid-js';
-import IconProgress from '~icons/material-symbols/progress-activity';
 
 import { useAdmin, useLocale } from '@/context';
 import styles from './style.module.css';
@@ -22,6 +21,6 @@ export function Logout(): JSX.Element {
 
     // 在网络不通时，ctx.logout 可能会非常耗时，所以此处展示一个简单的提示页面。
     return <Page title="_p.current.logout" class={styles.logout}>
-        <IconProgress class="animate-spin" />{l.t('_p.current.loggingOut')}
+        <IconCmfxBrandAnimate />{l.t('_p.current.loggingOut')}
     </Page>;
 }
