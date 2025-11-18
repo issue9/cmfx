2449534bff6a4ce57e8f8599eae7ebebea99a644
caixw <xnotepad@gmail.com>
2025-11-18 15:25:44 +0800

    refactor(docs): 删除了不必要的绽放按钮

--- apps/docs/src/demo/illustrations/429.tsx
+++ apps/docs/src/demo/illustrations/429.tsx
@@ -2,9 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { joinClass, MountProps } from '@cmfx/components';
+import { MountProps } from '@cmfx/components';
 import * as illustrations from '@cmfx/illustrations';
-import { createMemo } from 'solid-js';
 import { Portal } from 'solid-js/web';
 
 import { boolSelector, paletteSelector } from '../base';
@@ -12,19 +11,13 @@ import { boolSelector, paletteSelector } from '../base';
 export default function(props: MountProps) {
     const [paletteS, palette] = paletteSelector();
     const [customS, custom] = boolSelector('自定义文字内容', false);
-    const [scaleS, scale] = boolSelector('缩放', false);
-
-    const cls = createMemo(() => {
-        return joinClass(undefined, scale() ? 'w-200' : 'w-150', 'p-4', 'bg-palette-bg');
-    });
 
     return <>
         <Portal mount={props.mount}>
             {paletteS}
             {customS}
-            {scaleS}
         </Portal>
 
-        <illustrations.Error429 class={cls()} palette={palette()} text={custom() ? '请求过多' : undefined} />
+        <illustrations.Error429 class='bg-palette-bg aspect-square w-full' palette={palette()} text={custom() ? '请求过多' : undefined} />
     </>;
 }
--- apps/docs/src/demo/illustrations/bug.tsx
+++ apps/docs/src/demo/illustrations/bug.tsx
@@ -2,27 +2,20 @@
 //
 // SPDX-License-Identifier: MIT
 
-import { joinClass, MountProps } from '@cmfx/components';
+import { MountProps } from '@cmfx/components';
 import * as illustrations from '@cmfx/illustrations';
-import { createMemo } from 'solid-js';
 import { Portal } from 'solid-js/web';
 
-import { boolSelector, paletteSelector } from '../base';
+import { paletteSelector } from '../base';
 
 export default function(props: MountProps) {
     const [paletteS, palette] = paletteSelector();
-    const [scaleS, scale] = boolSelector('缩放', false);
-
-    const cls = createMemo(() => {
-        return joinClass(undefined, scale() ? 'w-200' : 'w-150', 'p-4', 'bg-palette-bg');
-    });
 
     return <>
         <Portal mount={props.mount}>
             {paletteS}
-            {scaleS}
         </Portal>
 
-        <illustrations.BUG class={cls()} palette={palette()} />
+        <illustrations.BUG class='bg-palette-bg aspect-square w-full' palette={palette()} />
     </>;
 }
