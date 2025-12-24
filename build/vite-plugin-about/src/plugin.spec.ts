a38dc59cb58a715d55c0bf0c6bbf763846545950
caixw <xnotepad@gmail.com>
2025-12-24 13:40:29 +0800

    test: 修正测试错误

--- build/vite-plugin-about/src/plugin.spec.ts
+++ build/vite-plugin-about/src/plugin.spec.ts
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
+import { aboutName } from '@cmfx/admin/plugin';
 import { expect, test } from 'vitest';
 
 import { about } from './plugin';
@@ -17,7 +18,7 @@ test('plugin', async () => {
     expect(typeof p.config).toEqual('function');
     if (typeof p.config === 'function') {
         const conf = await p.config!({}, { command: 'build', mode: 'prod' });
-        const obj = conf?.define!['__CMFX_ABOUT__'];
+        const obj = conf?.define![aboutName];
         expect(obj.dependencies.length).toEqual(0); // 根目录的 package.json 没有 dependencies。
         expect(obj.devDependencies.length).toBeGreaterThan(5);
         expect(obj.serverDependencies.length).toBeGreaterThan(5);
--- packages/components/src/datetime/timezone/timezone.spec.tsx
+++ packages/components/src/datetime/timezone/timezone.spec.tsx
@@ -5,14 +5,25 @@
 import { describe, expect, test } from 'vitest';
 
 import { ComponentTester } from '@/context/context.spec';
-import { buildLocaleRegion, default as Timezone } from './timezone';
+import { buildLocaleRegion, buildRegion, default as Timezone } from './timezone';
 
-test('buildLocaleRegion', async () => {
-    const enUS = buildLocaleRegion(new Intl.Locale('en-US'), 'full');
-    const zhHans = buildLocaleRegion(new Intl.Locale('zh-Hans'), 'full');
+describe('region', () => {
+    const regions = buildRegion();
 
-    expect(enUS.length).toEqual(zhHans.length);
-    expect(enUS.length).toBeGreaterThan(3);
+    test('buildRegion', () => {
+        expect(regions.length).toBeGreaterThan(3); // 每个浏览器的数量都不同，但是肯定是有数量的
+    });
+
+    test('buildLocaleRegion', () => {
+        const enUS = buildLocaleRegion(new Intl.Locale('en-US'), 'full');
+        const zhHans = buildLocaleRegion(new Intl.Locale('zh-Hans'), 'full');
+
+        const en = enUS.get(regions[0].timezones[0]);
+        const cn = zhHans.get(regions[0].timezones[0]);
+        expect(en).not.toEqual(cn);
+        expect(en).toBeDefined();
+        expect(cn).toBeDefined();
+    });
 });
 
 describe('Timezone', async () => {
